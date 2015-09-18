84f34d381cf11e253dabff4ffad2a6d8d65146a5
Rudradev Roy <rudradev.roy@gmail.com>
2015-09-18 17:30:57 -0400

    Add latest changes

--- Pitch Perfect/PlayUserSoundViewController.swift
+++ Pitch Perfect/PlayUserSoundViewController.swift
@@ -11,38 +11,64 @@ import AVFoundation
 
 class PlayUserSoundViewController: UIViewController {
     
-    var audioPlayer = AVAudioPlayer()
-    
     @IBOutlet weak var Button: UIButton! //snail button
     
-    override func viewDidLoad() {
+    @IBOutlet weak var SpeedUp: UIButton!
+    
+    @IBOutlet weak var stop1: UIButton!
+    
+    var audioPlayer = AVAudioPlayer()
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
         
-        if var filePath = NSBundle.mainBundle().pathForResource("movie_quote", ofType: "mp3"){
+        if var filePath = NSBundle.mainBundle().pathForResource("test", ofType: "mp3")
+        {
+            var song = NSURL.fileURLWithPath(filePath)
+            audioPlayer = AVAudioPlayer(contentsOfURL: song!, error:nil)
+            audioPlayer.enableRate=true
             
-             var song = NSURL.fileURLWithPath(filePath)
-             audioPlayer = AVAudioPlayer(contentsOfURL: song, error:nil)
-
         }
+            
         else
+            
         {
             print("Path is empty")
         }
-        
-       
         // Do any additional setup after loading the view.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    
+    override func didReceiveMemoryWarning()
+    {
+        super.didReceiveMemoryWarning() // Dispose of any resources that can be recreated.
     }
     
-    @IBAction func SlowMo(sender: UIButton) {
+    
+    
+    @IBAction func SlowMo(sender: UIButton)
+    {
+        audioPlayer.stop()
+        audioPlayer.rate = 0.5
+        audioPlayer.play()
         // Play slow audio
-         audioPlayer.play()
+        
+    }
+    @IBAction func IncreaseSpeed(sender: UIButton) {
+            audioPlayer.stop()
+            audioPlayer.rate = 1.5
+            audioPlayer.play()
     }
    
+    @IBAction func stop(sender: UIButton)
+    {
+        
+        audioPlayer.stop()
+    }
+   
+    
     /*
     // MARK: - Navigation
 
